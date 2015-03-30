dd93dccbde99d9bbd6d047d70ce8e2a7299e4602
Michael Karliner <mike@modern-industry.com>
2015-03-30 10:09:10 +0100

    FIX: remove colons in file names for Windows build

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mkarliner:mqtt',
-  version: '0.0.6',
+  version: '0.0.9',
   summary: 'Connect Meteor to MQTT message broker',
   git: 'http://www.github.com/mkarliner/meteor-mqtt',
   documentation: 'README.md'
@@ -14,12 +14,12 @@ Npm.depends({
 Package.onUse(function(api) {
   api.export("mqtt", "server");
   api.versionsFrom('1.0.3.1');
-  api.addFiles('mkarliner:server.js', ["server"]);
+  api.addFiles('server.js', ["server"]);
   api.addFiles(['browserMqtt.js'], "client");
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('mkarliner:mqtt');
-  api.addFiles('mkarliner:mqtt-tests.js');
+  api.addFiles('mqtt-tests.js');
 });
