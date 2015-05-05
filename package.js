6153b58d8161a1389e45d792a9e37c3b94d0efdf
Michael Karliner <mike@modern-industry.com>
2015-05-05 12:52:21 +0100

    BUMP: Version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mkarliner:mqtt',
-  version: '0.0.9',
+  version: '0.1.0',
   summary: 'Connect Meteor to MQTT message broker',
   git: 'http://www.github.com/mkarliner/meteor-mqtt',
   documentation: 'README.md'
